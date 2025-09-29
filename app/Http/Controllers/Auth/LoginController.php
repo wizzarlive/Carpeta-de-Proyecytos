4b79a4968cc1026d827a959583d7a63246fc329b
Huayapa <huayapajosue0@gmail.com>
2025-09-29 12:09:20 -0500

    nose

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -30,7 +30,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/proyectos';
 
     /**
      * Create a new controller instance.
