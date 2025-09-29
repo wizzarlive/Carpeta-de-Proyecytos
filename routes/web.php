f6d98ec1b73fff2306c9b94f43a917b3f3463195
wizzarlive <juniorminano080@gmail.com>
2025-09-29 08:58:44 -0500

    Npm

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get(
+    'login/google',
+    [\App\Http\Controllers\Auth\LoginController::class, 'redirectToGoogle']
+);
+
+Route::get(
+    'login/google/callback',
+    [\App\Http\Controllers\Auth\LoginController::class, 'handleGoogleCallback']
+);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
